d513fca62461320d13c8c5539770b62089a807b3
leoliveira1911 <leoliveira1911@gmail.com>
2022-08-17 15:32:19 -0300

    form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -32,30 +32,47 @@ export default function Form (props: FormProps) {
     }
 
     return(
-        <div>
-                <h3 style={{ marginTop: '50px' }} >{props.action}</h3>
+        <div className={`
+        flex flex-col items-center justify-center m-2 p-3 rounded-2xl overflow-hidden
+        bg-gray-900 
+        `}>
+                <h3 className={`
+                         p-1 font-bold text-xl
+                    `} >{props.action}</h3>
                  <form onSubmit={(e)=> props.submit(e)}>
-                    <div>
+                    <div className={`
+                        flex flex-col p-1 font-semibold
+                    `}>
                         <label htmlFor='date'> Data de compra</label>
-                        <input required value={date} onChange={e => {props.setDate(e.target.value) ; setDate(e.target.value)}} type="text" name="date" id="date" />
+                        <input placeholder="DD/MM/AAAA" className={`text-black rounded-md px-2 py-1`} required value={date} onChange={e => {props.setDate(e.target.value) ; setDate(e.target.value)}} type="text" name="date" id="date" />
                     </div>
-                    <div>
+                    <div className={`
+                        flex flex-col p-1 font-semibold
+                    `}>
                         <label htmlFor='company'> Empresa</label>
-                        <input required value={company} onChange={e => {props.setCompany(e.target.value) ; setCompany(e.target.value)}} type="text" name="company" id="company" />
+                        <input placeholder="Empresa ABC" className={`text-black rounded-md px-2 py-1`} required value={company} onChange={e => {props.setCompany(e.target.value) ; setCompany(e.target.value)}} type="text" name="company" id="company" />
                     </div>
-                    <div>
+                    <div className={`
+                        flex flex-col p-1 font-semibold
+                    `}>
                         <label htmlFor='ticker'> Ticker </label>
-                        <input required value={ticker} onChange={e => {props.setTicker(e.target.value) ; setTicker(e.target.value)}} type="text" name="ticker" id="ticker" />
+                        <input placeholder="TICK3" className={`text-black rounded-md px-2 py-1`} required value={ticker} onChange={e => {props.setTicker(e.target.value) ; setTicker(e.target.value)}} type="text" name="ticker" id="ticker" />
                     </div>
-                    <div>
+                    <div className={`
+                        flex flex-col p-1 font-semibold
+                    `}>
                         <label htmlFor='buyPrice'> Preço de compra </label>
-                        <input required value={buyPrice} onChange={e => {props.setBuyPrice(e.target.value) ; setBuyPrice(e.target.value)}} type="text" name="buyPrice" id="buyPrice" />
+                        <input placeholder="99,99" className={`text-black rounded-md px-2 py-1`} required value={buyPrice} onChange={e => {props.setBuyPrice(e.target.value) ; setBuyPrice(e.target.value)}} type="text" name="buyPrice" id="buyPrice" />
                     </div>
-                    <div>
+                    <div className={`
+                        flex flex-col p-1 font-semibold
+                    `}>
                         <label htmlFor='shares'> Número de ações </label>
-                        <input required value={shares} onChange={e => {props.setShares(e.target.value) ; setShares(e.target.value)}} type="text" name="shares" id="shares" />
+                        <input placeholder="100" className={`text-black rounded-md px-2 py-1`} required value={shares} onChange={e => {props.setShares(e.target.value) ; setShares(e.target.value)}} type="text" name="shares" id="shares" />
                     </div>
-                    <div>
+                    <div className={`
+                    flex justify-center
+                    `}>
                         {/* <button type='submit' onClick={()=>resetValues}>Enviar</button> */}
                         <Button type='submit' onClick={()=>resetValues} label='Enviar'></Button>
                     </div>
