b108e44813b4585703e04be8a33a305db879aa76
leoliveira1911 <leoliveira1911@gmail.com>
2022-08-18 11:50:50 -0300

    sm fixed 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   darkMode: 'class',
   theme: {
     screens: {
-      'sm': '400px',
+      'sm': '300px',
       // => @media (min-width: 640px) { ... }
 
       'first': '1350px',
